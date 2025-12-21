c13b3a841b22761720e0b9ba4cf3f022a46e1f06
faris Naser <solonaser9@gmail.com>
2025-12-20 18:12:16 -0800

    feat:add a Loading js file to perform Loading animation in the file uing  js

--- async_programing/loading.js
+++ async_programing/loading.js
@@ -2,8 +2,10 @@ let i = 5;
 const Loading = setInterval(() => {
   console.log("Loading ...");
   i -= 1;
-  if (i <= 0) {
-    console.log("Done after 5 seconds");
-    clearInterval(Loading);
-  }
 }, 1000);
+
+console.log("  five passed");
+setTimeout(() => {
+  console.log("  five  minuts passed ");
+  clearInterval(Loading);
+});
