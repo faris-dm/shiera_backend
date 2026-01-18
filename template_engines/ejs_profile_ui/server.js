5a3574ce03fc4eb621436017ff5bae0d155eb17e
faris Naser <solonaser9@gmail.com>
2026-01-17 16:49:49 -0800

    feat:created a get route with it port

---
+++ template_engines/ejs_profile_ui/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+let app = express();
+
+// app.set("view engine", "ejs");
+// app.use(express.static("public"));
+// app.get("/", (req, res) => {
+//   const hannsInfo = {
+//     name: "Fatima Noor",
+//     age: "23",
+//     profession: "Software Developer",
+//     skill: [" Node.js  |", " Express |", " MongoDB |", "\t EJS"],
+//     goal: "to be married  and  have  a family",
+//     img: "images",
+//     qoutes: "</ coder >",
+//   };
+//   res.render("hello", { hannsInfo });
+// });
+
+let port = 5200;
+app.listen(port, (req, res) => {
+  console.log(`https://localhost:${port}`);
+});
