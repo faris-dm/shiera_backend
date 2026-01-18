40718357c306dc7db9f06fc2637aefc347c56795
faris Naser <solonaser9@gmail.com>
2026-01-18 11:48:55 -0800

    feat:created a get router to send data to index.pug  that used for rendering

--- template_engines/pug_profile_ui/server.js
+++ template_engines/pug_profile_ui/server.js
@@ -1,9 +1,18 @@
 import express from "express";
 let app = express();
 app.set("view engine", "pug");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send(" this page is working");
+});
+
+app.get("/home", (req, res) => {
+  let data = {
+    message: "hello solo from your Fam",
+    reality: " you are alone and have you enjoy your life",
+  };
+  res.render("index", data);
 });
 
 let port = 7666;
